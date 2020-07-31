2534941e98e8592aa217b8d1a0c20d09ec2499e3
su-anshu <anshukumar.saha@gmail.com>
2020-07-31 23:47:34 +0530

    micropost changes

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,6 @@ class Micropost < ApplicationRecord
     def micropost_params
         params.require(:micropost).permit(:content, :user_id)
     end
-    belongs_to :user
+    belongs_to :user, :optional => true
     validates :content, :length => { :maximum => 140 }
 end
